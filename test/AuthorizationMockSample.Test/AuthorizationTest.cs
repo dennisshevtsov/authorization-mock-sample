f209a03bcbf94086414ee4c2563f96d8e23f4849
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-06 13:47:52 -0800

    add test

--- test/AuthorizationMockSample.Test/AuthorizationTest.cs
+++ test/AuthorizationMockSample.Test/AuthorizationTest.cs
@@ -2,13 +2,24 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using System.Net;
+using System.Net.Http.Headers;
+
 namespace AuthorizationMockSample.Test;
 
 [TestClass]
 public class AuthorizationTest
 {
   [TestMethod]
-  public void TestMethod1()
+  public async Task GetAsync_NoBearerToken_401Returned()
   {
+    // Arrange
+    using HttpClient client = new();
+
+    // Act
+    using HttpResponseMessage response = await client.GetAsync("http://localhost:5001");
+
+    // Assert
+    Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
   }
 }
